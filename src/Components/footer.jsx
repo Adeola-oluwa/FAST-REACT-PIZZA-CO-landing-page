844dd59e62db49adf1905762bb079a279f427dcb
adeola-oluwa <euniceadeola2006@gmail.com>
2024-02-19 11:25:16 +0100

    Your commit message here

--- src/Components/footer.jsx
+++ src/Components/footer.jsx
@@ -1,19 +1,30 @@
 import React from "react";
-import '../css/index.css'
+import "../css/index.css";
 
-const footer = () => {
+const Footer = () => {
+  const currentTime = new Date().getHours(); 
+
+  const isWithinOpeningHours = currentTime >= 10 && currentTime <= 22;
+
   return (
     <>
       <div className="order">
-        <h2 className="footer">
-          Our hours are from 12:00 to 22:00. Come visit us or order online
-        </h2>
-        {/* <div className="order"> */}
-          <button className="btn">Order</button>
-        {/* </div> */}
+      
+        {isWithinOpeningHours ? (
+          <>
+            <h2 className="footer">
+              We're open from 10:00 to 22:00. Come visit us or order online
+            </h2>
+            <button className="btn">Order</button>
+          </>
+        ) : (
+          <h2 className="footer">
+            We are happy to welcome you between 10:00 and 22:00
+          </h2>
+        )}
       </div>
     </>
   );
 };
 
-export default footer;
+export default Footer;
